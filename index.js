d04e1d159315e24f7f758a348151dd12d77d4c9b
Damien Le Thiec <damien.lethiec@gmail.com>
2019-03-11 11:02:36 +0100

    create node_proxy

---
+++ index.js
@@ -0,0 +1,10 @@
+var proxy = require('html2canvas-proxy');
+var express = require('express');
+
+var app = express();
+
+app.get('/', proxy())
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!')
+})
